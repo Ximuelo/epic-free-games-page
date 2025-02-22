6af5af79cc03244261773e460546463d57f6dfee
Ximuelo <xquintanaf.denia@gmail.com>
2025-02-22 20:19:03 +0100

    Header & Hero Sections

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ export function App() {
 	return (
 		<LocationProvider>
 			<main>
+                <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
 				<Router>
                     <Route path="/" component={Redirect} />
 					<Route path="/:country" component={Home} />
