9c794cca27faf3bd670c0a794085be965d426053
Ximuelo <xquintanaf.denia@gmail.com>
2025-03-01 20:25:53 +0100

    Adding cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-    output: "server", // Activate SSR
-    vite: {
-        plugins: [tailwindcss()], 
-    }
+  // Activate SSR
+  output: "server",
+
+  vite: {
+      plugins: [tailwindcss()], 
+  },
+
+  adapter: cloudflare()
 });
