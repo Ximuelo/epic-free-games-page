ed6fdb4936243cbd862092e09c5cd1c1d96ef2ef
Ximuelo <xquintanaf.denia@gmail.com>
2025-03-09 23:00:09 +0100

    Cleanup cloudflare adapter that doesnt work :D

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,11 @@ import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
 
-//import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   // Activate SSR
   output: "server",
 
-  //adapter: cloudflare(),
   vite: {
       plugins: [tailwindcss()], 
   },
